2714c2bfe2b414655f6170b0b2c5bad4699ee9a6
Taner Özer <tanerozer16@gmail.com>
2019-12-12 22:23:21 +0300

    change number

---
+++ sayıdegistirme/src/sayıdegistirme/main.java
@@ -0,0 +1,17 @@
+package sayýdegistirme;
+
+public class main {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=155;
+		b=101;
+		System.out.println("a:"+a+" b:"+b);
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println("a:"+a+" b:"+b);
+		
+	}
+
+}
