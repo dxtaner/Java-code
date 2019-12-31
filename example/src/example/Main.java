1dab530d1cb7d863e4712214ad21851980059ec5
Taner Özer <44675799+dxtaner@users.noreply.github.com>
2019-12-31 21:20:38 +0300

    example

---
+++ example/src/example/Main.java
@@ -0,0 +1,22 @@
+package example;
+//dxtaner
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Merhaba Dunya");
+		
+		byte value = 127;
+		System.out.println(value);
+		value += 1;
+		System.out.println(value); // byte value=128 compiler hata verir.
+		
+		System.out.println(Integer.MAX_VALUE);
+		
+		int x=(int) Math.pow(4,3);
+		System.out.println("X: "+x);
+		
+		
+	}
+
+}
