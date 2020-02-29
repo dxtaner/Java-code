53bc71a1b45c0c51ca92b8889d8f552b89370f1b
Taner Özer <44675799+dxtaner@users.noreply.github.com>
2020-02-29 19:15:53 +0300

    dosyalamaislemi

---
+++ Dosyaişlemleri/src/Eleman.java
@@ -0,0 +1,27 @@
+
+public class Eleman {
+	private String ad;
+	private String soyad;
+	private double maas;
+	
+	public String getAd() {
+		return ad;
+	}
+	public void setAd(String ad) {
+		this.ad = ad;
+	}
+	public String getSoyad() {
+		return soyad;
+	}
+	public void setSoyad(String soyad) {
+		this.soyad = soyad;
+	}
+	public double getMaas() {
+		return maas;
+	}
+	public void setMaas(double maas) {
+		this.maas = maas;
+	}
+	
+
+}
