f728948504c18c2db81fe8d9d4d93652c64bc6e8
Taner Özer <tanerozer16@gmail.com>
2020-04-02 17:03:26 +0300

    yildiz

---
+++ yildiz/src/yildiz/main.java
@@ -0,0 +1,17 @@
+package yildiz;
+//dxtaner
+public class main {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++)
+		{
+			for(int j=i;j<10;j++)
+			{
+				System.out.print("*\t"); 
+			}
+			System.out.println(); 
+		}
+
+	}
+
+}
