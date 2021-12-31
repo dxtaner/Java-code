5b5d9b7cc824b8bd4b95ffcb1de64aa1e8d72b78
Taner <tanerozer16@gmail.com>
2021-12-31 13:36:12 +0300

    TersUcgenyapimi

---
+++ ornekler/src/YıldızTersUcgen.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class YıldızTersUcgen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir sayi girin:");
+        int sayi = scanner.nextInt();
+        int n=sayi;
+
+        for (int i = n; i >=0; i--) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
