108563b60b1f8fc704241309cc2bd92e77e58424
Taner Özer <tanerozer16@gmail.com>
2022-04-10 00:57:31 +0300

    presonaltacking

---
+++ personaltracking/PersonalTracking/src/Fault.java
@@ -0,0 +1,22 @@
+
+public class Fault {
+	
+	private int faultID;
+	private String faultUnit;
+	
+	public int getFaultID() {
+		return faultID;
+	}
+	public void setFaultID(int faultID) {
+		this.faultID = faultID;
+	}
+	public String getFaultUnit() {
+		return faultUnit;
+	}
+	public void setFaultUnit(String faultUnit) {
+		this.faultUnit = faultUnit;
+	}
+	
+	
+	
+}
---
+++ personaltracking/PersonalTracking/src/Working.java
@@ -0,0 +1,39 @@
+
+public class Working {
+	
+	private String name;
+	private String surName;
+	private int salary;
+	private String department;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurName() {
+		return surName;
+	}
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	public double raiseCalculate()
+	{
+		return 0.04*salary;
+	}
+	
+}
