709fe87677184fb2b46ebd6952b714d31befe9ff
Taner Özer <tanerozer16@gmail.com>
2022-09-25 00:53:06 +0300

    javaexapmle2

---
+++ java2022/JavaExample2/src/ReCapDemo_Classes.java
@@ -0,0 +1,14 @@
+
+public class ReCapDemo_Classes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    MathFunctions calculator = new MathFunctions();
+        System.out.println(calculator.sum(4,5));
+        System.out.println(calculator.division(15,5));
+        System.out.println(calculator.decal(15,5));
+        System.out.println(calculator.multiplication(3, 0));
+
+	}
+
+}
