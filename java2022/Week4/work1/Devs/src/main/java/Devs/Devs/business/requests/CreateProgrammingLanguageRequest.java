cf2d20d8ad3eb87ac507d35d56ce028a25443d07
taner <tanerozer16@gmail.com>
2022-11-03 20:55:36 +0300

    Create CreateProgrammingLanguageRequest.java

---
+++ java2022/Week4/work1/Devs/src/main/java/Devs/Devs/business/requests/CreateProgrammingLanguageRequest.java
@@ -0,0 +1,14 @@
+package Devs.Devs.business.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateProgrammingLanguageRequest {
+
+	private String programmingLanguageName;
+
+}
