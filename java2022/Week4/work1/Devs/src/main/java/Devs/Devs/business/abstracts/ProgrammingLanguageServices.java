4ef96dadeb0178e139da5eb465b559344c3cf014
taner <tanerozer16@gmail.com>
2022-11-03 20:56:14 +0300

    Update ProgrammingLanguageServices.java

--- java2022/Week4/work1/Devs/src/main/java/Devs/Devs/business/abstracts/ProgrammingLanguageServices.java
+++ java2022/Week4/work1/Devs/src/main/java/Devs/Devs/business/abstracts/ProgrammingLanguageServices.java
@@ -2,12 +2,14 @@ package Devs.Devs.business.abstracts;
 
 import java.util.List;
 
+import Devs.Devs.business.requests.CreateProgrammingLanguageRequest;
+import Devs.Devs.business.responses.GetAllProgrrammingLanguagesResponse;
 import Devs.Devs.entities.concretes.ProgrammingLanguage;
 
 public interface ProgrammingLanguageServices {
-	List<ProgrammingLanguage> getAll();
-	ProgrammingLanguage getById(int id);
-	void add(ProgrammingLanguage programmingLanguage) throws Exception;
+	List<GetAllProgrrammingLanguagesResponse> getAll();
+	void add(CreateProgrammingLanguageRequest createProgrammingLanguageRequest) throws Exception;
 	void update(String name,int id) throws Exception;
 	void delete(int id) throws Exception;
+	ProgrammingLanguage getById(int id);
 }
