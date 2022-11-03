cf08ae80f4347e8f3b4571b47e1fc9c3fe50366c
taner <tanerozer16@gmail.com>
2022-11-03 20:56:09 +0300

    Update ProgrammingLanguageManager.java

--- java2022/Week4/work1/Devs/src/main/java/Devs/Devs/business/concretes/ProgrammingLanguageManager.java
+++ java2022/Week4/work1/Devs/src/main/java/Devs/Devs/business/concretes/ProgrammingLanguageManager.java
@@ -1,11 +1,14 @@
 package Devs.Devs.business.concretes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import Devs.Devs.business.abstracts.ProgrammingLanguageServices;
+import Devs.Devs.business.requests.CreateProgrammingLanguageRequest;
+import Devs.Devs.business.responses.GetAllProgrrammingLanguagesResponse;
 import Devs.Devs.dataAccess.abstracts.ProgrammingLanguageRepository;
 import Devs.Devs.entities.concretes.ProgrammingLanguage;
 
@@ -21,14 +24,26 @@ public class ProgrammingLanguageManager implements ProgrammingLanguageServices{
 	}
 
 	@Override
-	public List<ProgrammingLanguage> getAll() {
-		return programmingLanguageRepository.getAll();
+	public List<GetAllProgrrammingLanguagesResponse> getAll() {
+		
+		List<ProgrammingLanguage> programmingLanguages = programmingLanguageRepository.findAll();
+		List<GetAllProgrrammingLanguagesResponse> programmingLanguagesResponse = new ArrayList<GetAllProgrrammingLanguagesResponse>();
+		
+		for (ProgrammingLanguage programmingLanguage : programmingLanguages) {
+			
+			GetAllProgrrammingLanguagesResponse responseItem = new GetAllProgrrammingLanguagesResponse();
+			responseItem.setProgrammingLanguageId(programmingLanguage.getProgrammingLanguageId());
+			responseItem.setProgrammingLanguageName(programmingLanguage.getProgrammingLanguageName());
+			
+			programmingLanguagesResponse.add(responseItem);
+		}
+		return programmingLanguagesResponse;
 	}
 
 	@Override
 	public ProgrammingLanguage getById(int id) {
-		for (ProgrammingLanguage programmingLanguage : programmingLanguageRepository.getAll()) {
-			if(programmingLanguage.getId()==id) {
+		for (ProgrammingLanguage programmingLanguage : programmingLanguageRepository.findAll()) {
+			if(programmingLanguage.getProgrammingLanguageId()==id) {
 				return programmingLanguage;
 			}
 		}
@@ -36,33 +51,35 @@ public class ProgrammingLanguageManager implements ProgrammingLanguageServices{
 	}
 
 	@Override
-	public void add(ProgrammingLanguage programmingLanguage) throws Exception {
-		 if(programmingLanguage.getName().isEmpty() || programmingLanguage.getName().isBlank()) {
+	public void add(CreateProgrammingLanguageRequest createProgrammingLanguageRequest) throws Exception {
+		 if(createProgrammingLanguageRequest.getProgrammingLanguageName().isEmpty() || createProgrammingLanguageRequest.getProgrammingLanguageName().isBlank()) {
 			 throw new Exception("Name cannot be empty");
 		 }
-		 for (ProgrammingLanguage language : programmingLanguageRepository.getAll()) {
-            if (language.getName().equals(programmingLanguage.getName())) {
+		 for (ProgrammingLanguage language : programmingLanguageRepository.findAll()) {
+            if (language.getProgrammingLanguageName().equalsIgnoreCase(createProgrammingLanguageRequest.getProgrammingLanguageName())) {
             	throw new Exception("Name cannot be repeated");
             }	 
 		 }
-		 for (ProgrammingLanguage language : programmingLanguageRepository.getAll()) {
-	            if (language.getId()==programmingLanguage.getId()) {
-	            	throw new Exception("Id cannot be repeated");
-	            }	 
-		 }
-		 programmingLanguageRepository.add(programmingLanguage);
+//		 for (ProgrammingLanguage language : programmingLanguageRepository.findAll()) {
+//            if (language.getProgrammingLanguageId()==) {
+//            	throw new Exception("Id cannot be repeated");
+//            }	 
+//		 }
+		 ProgrammingLanguage programmingLanguage = new ProgrammingLanguage();
+		 programmingLanguage.setProgrammingLanguageName(createProgrammingLanguageRequest.getProgrammingLanguageName());
+		 programmingLanguageRepository.save(programmingLanguage);
 	}
 
 	@Override
 	public void update(String name, int id) throws Exception{
-		for (ProgrammingLanguage language : programmingLanguageRepository.getAll()) {
-            if (language.getName().equals(name)) {
+		for (ProgrammingLanguage language : programmingLanguageRepository.findAll()) {
+            if (language.getProgrammingLanguageName().equals(name)) {
             	throw new Exception("Name cannot be repeated");
             }	 
 		}
-		for (ProgrammingLanguage language : programmingLanguageRepository.getAll()) {
-            if (language.getId()==id) {
-	    		programmingLanguageRepository.update(name, id);
+		for (ProgrammingLanguage language : programmingLanguageRepository.findAll()) {
+            if (language.getProgrammingLanguageId()==id) {
+	    		programmingLanguageRepository.save(null);
             }	
 		}
 
@@ -70,7 +87,9 @@ public class ProgrammingLanguageManager implements ProgrammingLanguageServices{
 
 	@Override
 	public void delete(int id) throws Exception{
-		programmingLanguageRepository.delete(id); 
+		programmingLanguageRepository.deleteById(id); 
 	}
+
+
 
 }
