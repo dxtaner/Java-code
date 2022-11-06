d80a53be4e0bf9fd07ae185bdcea1570496c655d
taner <tanerozer16@gmail.com>
2022-11-06 15:02:11 +0300

    Create GetByIdSubTechResponse.java

---
+++ java2022/Week5/Devs/src/main/java/Devs/Devs/business/responses/subTechResponses/GetByIdSubTechResponse.java
@@ -0,0 +1,16 @@
+package Devs.Devs.business.responses.subTechResponses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetByIdSubTechResponse {
+
+	private int subTechId;
+	private String subTechName;
+	private int programmingLanguageId;
+
+}
