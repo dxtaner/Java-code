ab46e627e394ed5fce0333674edb2ffbcde79629
taner <tanerozer16@gmail.com>
2022-11-06 14:34:02 +0300

    Create DeleteProgrammingLanguageRequest.java

---
+++ java2022/Week5/Devs/src/main/java/Devs/Devs/business/requests/DeleteProgrammingLanguageRequest.java
@@ -0,0 +1,14 @@
+package Devs.Devs.business.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteProgrammingLanguageRequest {
+
+	private int programmingLanguageId;
+
+}
