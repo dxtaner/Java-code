48baef584059428b8443ccc53250dcfa2f0bcb82
taner <tanerozer16@gmail.com>
2023-12-08 17:55:17 +0300

    Create Marka.java

---
+++ ExampleVariuos/Store/Marka.java
@@ -0,0 +1,17 @@
+public class Marka {
+    private int id;
+    private String name;
+
+    public Marka(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
